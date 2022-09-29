94a02e4d481c597714c3f799fd4a65b33ae2a333
Daniel Han <DanielHan@pcl.com>
2022-09-29 09:05:22 -0600

    Initial Commit

---
+++ Walking Gamification System/Walking Gamification System/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Walking_Gamification_System.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
